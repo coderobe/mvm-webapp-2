9141af2a2d21179933f6f43dbb4ddaf8e8638116
Robin Broda <robin@broda.me>
2016-04-25 00:47:39 +0200

    moves join fallback timeout inside of join fallback

--- src/app.js
+++ src/app.js
@@ -121,8 +121,8 @@ app.do.run = () => {
         let al = data.DESCRIPTION
         al = al.charAt(0) + al.toLowerCase().slice(1)
         app.notification.alert(2, al, 2)
-        await psleep("2 seconds")
         if(data.NUMBER >= 10 && data.NUMBER < 20){
+          await psleep("2 seconds")
           app.do.select.lobby()
         }
         break
